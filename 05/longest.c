528076593e0c9aa73e13ea4af42e6193e8dd6501
Karol Moroz <karol@moroz.dev>
2025-02-04 23:33:11 +0800

    Add longest string finder

--- 05/longest.c
+++ 05/longest.c
@@ -1,51 +1,29 @@
 #include <stdio.h>
-
-#define MAXLINE 1000 /* maximum input line size */
-
-int getline(char line[], int maxline);
-void copy(char to[], char from[]);
+#include <stdlib.h>
+#include <string.h>
 
 /* print longest input line */
 int main() {
-  int len;               /* current line length */
-  int max;               /* maximum length seen so far */
-  char line[MAXLINE];    /* current input line */
-  char longest[MAXLINE]; /* longest line saved here */
+  char *line = NULL; /* current input line */
+  char *longest = NULL;
+  size_t len = 0, max = 0, nread = 0, bufsize = 0;
 
-  max = 0;
-  while ((len = getline(line, MAXLINE)) > 0) {
-    if (len > max) {
-      max = len;
-      copy(longest, line);
+  while ((nread = getline(&line, &len, stdin)) != -1) {
+    if (nread > max) {
+      max = nread;
+      if (len > bufsize) {
+        longest = realloc(longest, len);
+        bufsize = len;
+      }
+      strcpy(longest, line);
     }
   }
+  free(line);
+
   if (max > 0) {
-    printf("%s", longest);
+    printf("%ld\n %s", max, longest);
   }
+
+  free(longest);
   return 0;
-}
-
-/* getline: read a line into s, return length */
-int getline(char s[], int lim) {
-  int c, i;
-
-  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
-    s[i] = c;
-  }
-  if (c == '\n') {
-    s[i] = c;
-    ++i;
-  }
-  s[i] = '\0';
-  return i;
-}
-
-/* copy: copy `from` into `to`; assume to is big enough */
-void copy(char to[], char from[]) {
-  int i;
-
-  i = 0;
-  while ((to[i] = from[i]) != '\0') {
-    i++;
-  }
 }
