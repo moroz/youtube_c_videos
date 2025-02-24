c261a9a824ffa1086ec7033a0b037de8b0621c4e
Karol Moroz <karol@moroz.dev>
2025-02-25 02:47:34 +0800

    WIP on TOTP slides

--- exp/totp/slides/slides.js
+++ exp/totp/slides/slides.js
@@ -1,3 +1,5 @@
+import dayjs from "https://esm.sh/dayjs@latest";
+
 async function loadSlides() {
   const promises = [];
 
@@ -26,7 +28,8 @@ class PPTController {
     loadSlides()
       .then(this.detectSections)
       .then(this.setupKeyHandlers)
-      .then(() => this.setSlide(0));
+      .then(() => this.setSlide(0))
+      .then(() => this.setupUnixCounters());
   }
 
   detectSections = () => {
@@ -78,6 +81,39 @@ class PPTController {
       }
     });
   };
+
+  setupUnixCounters() {
+    const unixContainers = [...document.querySelectorAll("[data-unix]")];
+    const stepContainers = [...document.querySelectorAll("[data-step]")];
+    const timeContainers = [...document.querySelectorAll("[data-time]")];
+    const stepHexContainers = [...document.querySelectorAll("[data-step-hex]")];
+
+    setInterval(() => {
+      const now = new Date();
+      const unixepoch = Math.floor(Number(now) / 1000);
+      const t = Math.floor(unixepoch / 30);
+      for (const unix of unixContainers) {
+        unix.textContent = String(unixepoch);
+      }
+      for (const t of timeContainers) {
+        t.textContent = dayjs(now).format("YYYY-MM-DD HH:mm:ss");
+      }
+      for (const step of stepContainers) {
+        step.textContent = String(t);
+      }
+      const buf = new ArrayBuffer(8);
+      const view = new DataView(buf);
+      view.setUint32(4, t, false);
+      const bytes = Array.from(new Uint8Array(buf));
+
+      for (const hex of stepHexContainers) {
+        const cells = [...hex.querySelectorAll("td")];
+        for (let i = 0; i < 8; i++) {
+          cells[i].textContent = bytes[i].toString(16).padStart(2, "0");
+        }
+      }
+    }, 1000);
+  }
 }
 
 new PPTController();
