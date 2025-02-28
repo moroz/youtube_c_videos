9d48520a0b9a9f04d905d6cfa6f3d883b36969ae
Karol Moroz <karol@moroz.dev>
2025-02-28 11:58:39 +0800

    Cross-platform compilation

--- exp/totp/main.c
+++ exp/totp/main.c
@@ -1,27 +1,15 @@
-#include <time.h>
 #include <openssl/evp.h>
 #include <openssl/hmac.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 #include <time.h>
-#include <stdio.h>
 
 int base32_decode(const char *src, unsigned char *dst);
 
-int decode_hex(const char *hex, unsigned char *dst) {
-  for (int i = 0, n = strlen(hex) / 2; i < n; i++) {
-    unsigned int byte;
-    sscanf(hex + 2 * i, "%02x", &byte);
-    dst[i] = byte & 0xFF;
-  }
-
-  return 0;
-}
-
 void format_step(uint64_t step, uint8_t *dst) {
   for (int i = 0; i < 8; i++) {
-    uint8_t byte = (step & 0xFFUL << ((7 - i) * 8)) >> ((7 - i) * 8);
-    dst[i] = byte;
+    dst[i] = (step >> (7 - i) * 8) & 0xFF;
   }
 }
 
@@ -32,16 +20,25 @@ long int unixepoch() {
 }
 
 int main() {
-  const char *key_base32 = "PHAD43OB23JAWG65LBKHQNHNTXHST66F";
+  const char *key_base32 = "VXYLMUZAMMRWT5LFWBVDXCLYPJAMPZ5A";
   uint8_t key[20];
-  uint8_t digest[16];
+  uint8_t digest[20];
   base32_decode(key_base32, key);
+  for (int i = 0; i < 20; i++) {
+    printf("%02X ", key[i]);
+  }
+  putchar('\n');
 
   uint64_t t = unixepoch() / 30;
   uint8_t step_formatted[8];
   uint32_t len;
   format_step(t, step_formatted);
   HMAC(EVP_sha1(), key, 20, step_formatted, 8, digest, &len);
+
+  for (int i = 0; i < 20; i++) {
+    printf("%02X ", digest[i]);
+  }
+  putchar('\n');
 
   int offset = digest[len - 1] & 0xF;
   int binary = ((digest[offset] & 0x7F) << 24) |
@@ -50,4 +47,6 @@ int main() {
 
   int otp = binary % 1000000;
   printf("%06d\n", otp);
+
+  return 0;
 }
